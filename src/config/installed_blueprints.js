8b6b8364987f077b1133164d68c8a9e714179faf
Crowdbotics <team@crowdbotics.com>
2021-02-08 16:58:11 +0000

    remove package blueprint-react-native-articles-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,7 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
 { name: 'CopyOfArticleList202257', human_name: 'Copy of Article List', access_route: 'CopyOfArticleList202257'},
-{ name: 'ArticleList202236', human_name: 'Article List', access_route: 'ArticleList202236'},
 { name: 'ArticleList202235', human_name: 'Article List', access_route: 'ArticleList202235'},
 { name: 'ArticleList202234', human_name: 'Article List', access_route: 'ArticleList202234'},
 
